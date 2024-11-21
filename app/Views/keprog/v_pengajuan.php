f2cfe72b3f56292a0c8f347991e46655949c16e3
nandoperd <nandoperd@gmail.com>
2024-11-22 02:24:37 +0700

    ubah judul menu keprog

--- app/Views/keprog/v_pengajuan.php
+++ app/Views/keprog/v_pengajuan.php
@@ -106,7 +106,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Data Labkom</h1>
+            <h1>Data Pengajuan Barang</h1>
           </div>
         </div>
       </div><!-- /.container-fluid -->
